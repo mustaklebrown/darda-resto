7d860eb93a948ff27ab0dc159c53fbd230bbf946
mustaklebrown <embdoullah199@gmail.com>
2025-12-30 17:44:04 +0300

    feat: Add admin categories page for displaying and creating categories.

--- app/(dashbord)/admin/categories/page.tsx
+++ app/(dashbord)/admin/categories/page.tsx
@@ -1,48 +1,12 @@
-// import CategoryForm from '@/app/_components/category-form'
-// import prisma from '@/lib/prisma'
 
-
-// export default async function CategoriesPage() {
-//     const categories = await prisma.category.findMany({
-//         orderBy: { createdAt: 'asc' },
-//     })
-
-//     return (
-//         <div>
-//             <h1 className="text-3xl font-semibold mb-8">
-//                 Catégories
-//             </h1>
-
-//             <CategoryForm />
-
-//             <div className="mt-10 space-y-4">
-//                 {categories.map(cat => (
-//                     <div
-//                         key={cat.id}
-//                         className="
-//               flex items-center justify-between
-//               rounded-2xl border border-border/40
-//               bg-background/70 dark:bg-card/60
-//               backdrop-blur-xl p-6
-//             "
-//                     >
-//                         <span>{cat.name}</span>
-//                         <span className="text-muted-foreground text-sm">
-//                             {cat.slug}
-//                         </span>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     )
-// }
 
 import prisma from '@/lib/prisma'
 import { CategoryTile } from './category-tile'
 import { CategoryForm } from './category-form'
+import { Category } from '@/app/generated/prisma'
 
 export default async function CategoriesPage() {
-    const categories = await prisma.category.findMany({
+    const categories: Category[] = await prisma.category.findMany({
         orderBy: { createdAt: 'desc' },
     })
 
